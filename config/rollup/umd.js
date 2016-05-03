fdac4b02902a049b92529d2ee061a1f3d6a856d6
Comandeer <vepomoc@gmail.com>
2016-05-03 19:52:44 +0200

    [ci skip] Updated UMD module name in rollup configuration.

--- config/rollup/umd.js
+++ config/rollup/umd.js
@@ -3,7 +3,7 @@ import config from './es6.js';
 const packageInfo = require( '../../package.json' );
 
 config.format = 'umd',
-config.moduleName = 'bemquerySelectorEngine';
+config.moduleName = 'bemquery';
 config.dest = `dist/${packageInfo.name}.umd.js`;
 
 export default config;
